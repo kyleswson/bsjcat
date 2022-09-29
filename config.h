660e7070b1cf34594672f6c6330ba3983a6e05c8
kyleswson <114544481+kyleswson@users.noreply.github.com>
2022-09-30 01:51:28 +0900

    Update config.h

--- config.h
+++ config.h
@@ -34,7 +34,7 @@
 #define ENCODERS_PAD_B { D5 }
 #define ENCODERS_PAD_A_RIGHT { D2 }
 #define ENCODERS_PAD_B_RIGHT { D5 }
-#define ENCODER_RESOLUTION 2
+#define ENCODER_RESOLUTION { 2, 4 }
 
 /* Set 0 if debouncing isn't needed */
 #define DEBOUNCE 5
